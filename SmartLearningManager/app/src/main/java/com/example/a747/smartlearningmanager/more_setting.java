78a0b92432a163a36f911e88d366172a723f9b3b
747 <kof_31391@hotmail.com>
2016-08-28 22:14:28 +0700

    Almost finish todo
    need work on date and time
    delete fixed

--- SmartLearningManager/app/src/main/java/com/example/a747/smartlearningmanager/more_setting.java
+++ SmartLearningManager/app/src/main/java/com/example/a747/smartlearningmanager/more_setting.java
@@ -2,7 +2,6 @@ package com.example.a747.smartlearningmanager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInstaller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +15,7 @@ public class more_setting extends AppCompatActivity {
     }
 
     public void gotoTodo(View v){
-        Intent intent = new Intent(this, Todo.class);
+        Intent intent = new Intent(this, Todo_List.class);
         startActivity(intent);
     }
     public void gotoSetting(View v){
