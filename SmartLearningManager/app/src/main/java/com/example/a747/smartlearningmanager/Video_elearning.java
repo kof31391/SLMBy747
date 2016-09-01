31789604d135b6489e77436bcf919514fbcf5008
wasan081 <wasan.hi@hotmail.com>
2016-09-01 19:25:34 +0700

    interface Video 1/9/2016

---
+++ SmartLearningManager/app/src/main/java/com/example/a747/smartlearningmanager/Video_elearning.java
@@ -0,0 +1,13 @@
+package com.example.a747.smartlearningmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Video_elearning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_elearning);
+    }
+}
