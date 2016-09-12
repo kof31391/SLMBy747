b47246678ba5f6da21df82e9edf1fe5815c7817b
wasan081 <wasan.hi@hotmail.com>
2016-09-12 14:28:25 +0700

    subjectelearn

---
+++ SmartLearningManager/app/src/main/java/com/example/a747/smartlearningmanager/Subject_elearn.java
@@ -0,0 +1,13 @@
+package com.example.a747.smartlearningmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Subject_elearn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subject_elearn);
+    }
+}
