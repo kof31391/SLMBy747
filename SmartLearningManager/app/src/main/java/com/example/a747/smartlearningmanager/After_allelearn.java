b2bd5c22c61dbf85331a6f9ac4e12874fef1acf9
wasan081 <wasan.hi@hotmail.com>
2016-10-10 21:08:46 +0700

    after_all_elearn

---
+++ SmartLearningManager/app/src/main/java/com/example/a747/smartlearningmanager/After_allelearn.java
@@ -0,0 +1,13 @@
+package com.example.a747.smartlearningmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class After_allelearn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.after_allelearn);
+    }
+}
